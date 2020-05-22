14ea3a980c1652158adc19530a6827823a7d0d43
debutler266 <32465329+debutler266@users.noreply.github.com>
2020-05-22 12:47:40 -0400

    2nd bodyparser is working, logging the data when submitted in the form on index page. Form created for newsletter with bootswatch & font awesome styling

--- app.js
+++ app.js
@@ -5,8 +5,17 @@ const path = require('path');
 
 const app = express();
 
+// Middleware needed to use bodyParser
+app.use(bodyParser.urlencoded({extended: true}));
+
 // Declare static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Signup Route
+app.post('/signup', (req, res) => {
+  console.log(req.body)
+  res.send('hello');
+});
 
 const PORT = process.env.PORT || 5000;
 
