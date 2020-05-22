6e93fb2c8908961ca63e046dfcd5614e315c2ff9
debutler266 <32465329+debutler266@users.noreply.github.com>
2020-05-22 16:19:59 -0400

    3rd commit. app working. Adding the email entered to the mailchimp subscribtion list

--- app.js
+++ app.js
@@ -6,15 +6,59 @@ const path = require('path');
 const app = express();
 
 // Middleware needed to use bodyParser
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true}));
 
 // Declare static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Signup Route
 app.post('/signup', (req, res) => {
-  console.log(req.body)
-  res.send('hello');
+  const { firstName, lastName, email } = req.body;
+
+  // checks to make sure ALL fields are filled, if not will redirect to failed page
+  if (!firstName || !lastName || !email) {
+    res.redirect('/fail.html');
+    return;
+  }
+
+  // Data we send with request
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: 'subscribed',
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+  // turn objects (above) into strings, this data Cant be sent as objects
+  const postData = JSON.stringify(data);
+
+  const options = {
+    url: 'https://us18.api.mailchimp.com/3.0/lists/7587143c25',
+    method: 'POST',
+    headers: {
+      Authorization: 'auth b8d5572b461a7507d65fae2975d4cd26-us18'
+    },
+    body: postData
+  };
+
+  // Callback arrow function
+  request(options, (err, response, body) => {
+    if (err) {
+      res.redirect('/fail.html');
+    } else {
+      if(response.statusCode === 200) {
+        res.redirect('/success.html');
+      } else {
+        res.redirect('/fail.html');
+      }
+    }
+  });
 });
 
 const PORT = process.env.PORT || 5000;
